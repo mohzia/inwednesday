f06a4c7d80d8dd5e738e751e3109e288086a3f8b
mohsen ziaei <100121998+mohzia@users.noreply.github.com>
2022-02-27 19:02:14 +0330

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,5 +1,5 @@
-a =int(input ("please enter length :"))
-b =int(input ("please enter show :"))
-Environment=(a+b)*2
-area=a*b
+a =int(input ("please enter  side of the square :"))
+Environment=a*4
+area=a*a
 print("environment is : ",Environment," and area is : ",area)
+
